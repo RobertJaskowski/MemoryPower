aecb7ea71f9f3cc870e51c6e31ba642970443145
RobertJaskowski <robertjanjaskowski@gmail.com>
2018-01-17 22:23:08 +0100

    firebase update trying to implement

--- app/src/main/java/rj/pl/memorypower/Ranking.java
+++ app/src/main/java/rj/pl/memorypower/Ranking.java
@@ -2,13 +2,28 @@ package rj.pl.memorypower;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,20 +31,48 @@ import butterknife.OnClick;
 
 public class Ranking extends Activity {
 
-    @BindView(R.id.buttonTEMP)
-    Button buttonAdd;
+    @BindView(R.id.buttonTEMP1)
+    Button buttonAddOne;
+
+    @BindView(R.id.buttonTEMP2)
+    Button buttonAddTwo;
 
     @BindView(R.id.buttonUpdate)
     Button buttonUpdate;
 
+
+    @BindView(R.id.scoreTEMP)
+    EditText scoreTempUp;
+
+
+    @BindView(R.id.buttonTEMP3)
+    Button buttonUpdateOne;
+
+    @BindView(R.id.buttonTEMP4)
+    Button buttonUpdateTwo;
+
+    @BindView(R.id.buttonUpdate2)
+    Button buttonUpdate2;
+
+
+    @BindView(R.id.scoreTEMP2)
+    EditText scoreTempDown;
+
+
     @BindView(R.id.ranking)
     ListView listView;
 
-    @BindView(R.id.scoreTEMP)
-    EditText scoreTemp;
-
-
     private DatabaseReference myRef;
+
+    private boolean dataCheck;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseUser currentUser;
+
+
+
+    ArrayList<String> userlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,37 +85,175 @@ public class Ranking extends Activity {
         myRef = database.getReferenceFromUrl("https://memorypower-3ada5.firebaseio.com/");
 
 
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    @OnClick(R.id.buttonTEMP)
-    void addScore() {
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        currentUser = mAuth.getCurrentUser();
+
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.e("sign ","sign in success");
+                    showList();
+                }else{
+                  Log.e("sign ","failed");
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void showList() {
+
+        Query query = myRef.orderByChild("score").limitToLast(5);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+//                Log.e("tad", String.valueOf(User.score)+" "+ User.name);
+
+                userlist = new ArrayList<String>();
+
+                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+
+                    User score = dsp.getValue(User.class);
+                    userlist.add(score.name);
+                    Log.e("tad", String.valueOf(score.name));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+//        for (String u:userlist) {
+//            Log.e("tz",u);
+//
+//        }
+
+    }
+
+    @OnClick(R.id.buttonTEMP1)
+    void addScoreToNumbers() {
+        Integer score = Integer.parseInt(scoreTempUp.getText().toString());
+        String name = String.valueOf(scoreTempDown.getText());
+        if (score==0)
+            score = 3;
+
+        if (name.equals(""))
+            name= "test";
+
+
+
+        Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
         String key = myRef.child("numbers").push().getKey();
-        testObj testObj = new testObj("Rob", 5, 35);
+
+        User testObj = new User(name, score, 35);
+//        myRef.child("numbers").child(currentUser.getUid()).setValue(User);  //todo this works with anonymous auth , its overriding data
         myRef.child("numbers").child(key).setValue(testObj);
+
     }
 
-    @OnClick(R.id.buttonUpdate)
-    void UpdateScore(){
-        Toast.makeText(this,"updatin",Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.buttonTEMP2)
+    void addScoreToWords() {
+
+        Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+        String key = myRef.child("words").push().getKey();
+        User testObj = new User("Rob", 5, 35);
+        myRef.child("words").child(currentUser.getUid()).setValue(testObj);
+    }
+
+    @OnClick(R.id.buttonTEMP3)
+    void UpdateScoreNumbers() {
+        Toast.makeText(this, "updatin numbers", Toast.LENGTH_SHORT).show();
+        String key = myRef.child("numbers").getKey();
+        String userKey = myRef.child("numbers").push().getKey();
+        User testObj = new User("Tom", 6, 56);
+        Map<String, Object> postValues = testObj.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+//        childUpdates.put(key+ "/" + userKey, postValues); //creates new in numbers with key > data
+//        childUpdates.put(key, postValues); // this delates old from numbers but doesnt create user folder,  numbers> data
+        childUpdates.put(key+"/"+ currentUser.getUid()+"/", postValues);
+
+
+        myRef.updateChildren(childUpdates);
+
+
+//        if (checkIfFirebaseHasData(key)){
+//            Toast.makeText(this,"has data",Toast.LENGTH_SHORT).show();
+//        }
+
+
+    }
+
+    boolean checkIfFirebaseHasData(String key) {
+
+
+        myRef.child("numbers").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    dataCheck = true;
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return dataCheck;
 
     }
 
 }
 
-class testObj {
+class User {
     public String name;
     public int score;
     public int time;
 
-    public testObj() {
+    public User() {
     }
 
-    public testObj(String name, int score, int time) {
+    public User(String name, int score, int time) {
         this.name = name;
         this.score = score;
         this.time = time;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("score", score);
+        result.put("time", time);
+        return result;
     }
 
 }
